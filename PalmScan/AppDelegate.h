dc6d6f2a1aa943a239e89ebb4102644bc3b5859f
Realank <realank@126.com>
2015-11-25 12:52:36 +0800

    Initial Commit

---
+++ PalmScan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PalmScan
+//
+//  Created by Realank on 15/11/25.
+//  Copyright © 2015年 Realank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
